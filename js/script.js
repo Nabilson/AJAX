d4ee52165eb7a7c28abd0cdf47d4baf4c1a682f6
Nabilson <nabilson27@gmail.com>
2017-03-09 22:39:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		var name = "";
 
 		//Call server to get the name
-		$ajaxUtils.sendGetRequest("/data/name.txt", function (request){
+		$ajaxUtils.sendGetRequest("AJAX/data/name.txt", function (request){
 				var name = request.responseText;
 				document.querySelector("#content")
 					.innerHTML = "<h2>Hello " + name + "!"
